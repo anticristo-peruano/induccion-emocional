178ff0ebcc7774520f35aa29c380be19c6abf3a8
sofos <isoschronos@gmail.com>
2025-06-09 15:54:54 -0500

    Poco importante, pero mejoré el algoritmo de descarga para que no sea secuencial :p

--- functions/utils.py
+++ functions/utils.py
@@ -3,11 +3,12 @@ import pandas as pd
 import yt_dlp
 import os
 from tqdm import tqdm
-
-need_cols = ["track_id", "track_name", "valence",
-             "energy", "danceability", "popularity"]
+from multiprocessing import Pool
 
 def open_process(CSV_FILE,SEED,N_TRACKS):
+    need_cols = ["track_id", "track_name", "valence",
+             "energy", "danceability", "popularity"]
+
     if os.path.isfile(CSV_FILE):
         df_raw = pd.read_csv(CSV_FILE)
     else:
@@ -42,12 +43,13 @@ def open_process(CSV_FILE,SEED,N_TRACKS):
 
     return df, sim_mat
 
-def spotify_download(csv):
-    df = pd.read_csv('.data/dataset.csv')
+def download_process(args):
+    id, artist, track = args
 
-    for (_, (id, artist, track)) in tqdm(df[['track_id', 'artists', 'track_name']].iterrows()):
+    if os.path.exists(os.path.join('.songs',id + '.mp3')):
+        pass
+    else:
         track_name = f'{track} by {artist}'.replace("/","_")
-
         ydl_opts = {
                     'format': 'bestaudio/best',
                     'postprocessors': [{
@@ -57,10 +59,24 @@ def spotify_download(csv):
                     }],
                     'outtmpl': os.path.join('.songs', f"{id}.%(ext)s"),
                     'default_search': 'ytsearch',
+                    'quiet': True,
+                    'no_warnings': True,
+                    'progress_hooks': [],
+                    'logger': type('NopLogger', (), {'debug': lambda *a, **k: None, 'info': lambda *a, **k: None, 'warning': lambda *a, **k: None, 'error': lambda *a, **k: None})()
                 }
-        
+
         try:
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 ydl.download(track_name)
         except Exception as e:
-            print(f'Error downloading {track_name}: {e}')
+            with open('.songs/.errors.txt','a') as f:
+                f.write(f'Error downloading {id} - {track_name}: {e}\n')
+
+def pseudospotify_downloader(csv_file,subprocesses=6):
+    os.makedirs('.songs',exist_ok=True)
+    df = pd.read_csv(csv_file)[['track_id', 'artists', 'track_name']].values.tolist()
+
+    with Pool(processes=subprocesses) as p, tqdm(total=len(df)) as pbar:
+        for _ in p.imap(download_process,df):
+            pbar.update()
+            pbar.refresh()
